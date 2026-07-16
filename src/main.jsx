741bf5bc46cf6893b1e2f2ec7febef6e0c259716
dejin.li <lidejin@liduoxing.com>
2026-07-16 11:12:06 +0800

    feat: conditionally hide analytics and cloud features in Electron build

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import en_US from "@douyinfe/semi-ui/lib/es/locale/source/en_US";
 import "./index.css";
 import "./i18n/i18n.js";
 
+const isElectron = import.meta.env.VITE_ELECTRON === "true";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <LocaleProvider locale={en_US}>
     <App />
-    <Analytics />
+    {!isElectron && <Analytics />}
   </LocaleProvider>,
 );
